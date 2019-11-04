d8e33dc4af92888568b16454f32c0864e4e2e426
Mihaela Mesarosiu <mmesarosiu@macadamian.com>
2019-11-04 22:10:28 +0200

    Input field

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import SbyButton from './components/button';
 import theme from './styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Box my={2}>
+      <Box m={2}>
         <SbyButton variant="contained" size="xs">xs</SbyButton>
         <SbyButton variant="contained" size="sm">sm</SbyButton>
         <SbyButton variant="contained">normal</SbyButton>
@@ -15,7 +19,7 @@ function App() {
         <SbyButton variant="contained">work on your next listing</SbyButton>
       </Box>
 
-      <Box my={2}>
+      <Box m={2}>
         <SbyButton variant="outlined" size="xs">xs</SbyButton>
         <SbyButton variant="outlined" size="sm">sm</SbyButton>
         <SbyButton variant="outlined">normal</SbyButton>
@@ -23,12 +27,40 @@ function App() {
         <SbyButton variant="outlined">work on your next listing</SbyButton>
       </Box>
 
-      <Box my={2}>
+      <Box m={2}>
         <SbyButton variant="contained" color="secondary" size="xs">xs</SbyButton>
         <SbyButton variant="contained" color="secondary" size="sm">sm</SbyButton>
         <SbyButton variant="contained" color="secondary">normal</SbyButton>
         <SbyButton variant="contained" color="secondary" size="lg">lg</SbyButton>
         <SbyButton variant="contained" color="secondary">work on your next listing</SbyButton>
+      </Box>
+
+      <Box m={2}>
+        <OutlinedInput placeholder="Bare" variant="outlined" />
+        <OutlinedInput defaultValue="Type" disabled={true} placeholder="Bare" variant="outlined" />
+      </Box>
+
+      <Box m={2}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={true}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Active"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={false}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Normal"
+        />
       </Box>
     </ThemeProvider>
   );
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,11 +2,17 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const colors = {
   primary: '#0EA937',
-  secondary: '#C3C3C3'
+  secondary: '#C3C3C3',
+  lightGrey: 'rgba(0, 0, 0, 0.33)',
+  darkGrey: '#616161',
 };
 
+const inputColor = colors.lightGrey;
+
+const spacingBaseUnit = 5;
+
 const theme = createMuiTheme({
-  spacing: 5,
+  spacing: spacingBaseUnit,
   palette: {
     type: 'light',
     primary: {
@@ -17,6 +23,37 @@ const theme = createMuiTheme({
       main: colors.secondary,
       contrastText: '#616161',
     }
+  },
+  overrides: {
+    MuiOutlinedInput: {
+      focused: {
+        borderColor: colors.primary,
+        caretColor: colors.primary,
+      },
+      root: {
+        '&:hover $notchedOutline': {
+          borderColor: inputColor,
+        },
+      },
+      input: {
+        color: colors.darkGrey,
+        padding: `${2*spacingBaseUnit}px`,
+        '&::placeholder': {
+          color: inputColor,
+        },
+        '&:focus': {
+          color: colors.primary,
+        },
+        '&:disabled': {
+          backgroundColor: colors.secondary,
+        }
+      },
+    },
+    MuiCheckbox: {
+      root: {
+        color: inputColor,
+      },
+    },
   },
 });
 
