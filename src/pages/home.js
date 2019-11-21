71e41f2e67803e298b9612f8d2a6200e52412c60
Mihaela Mesarosiu <mmesarosiu@macadamian.com>
2019-11-21 17:42:13 +0200

    Styled html and grey body

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import SbyButton from 'components/button';
 
 const Home = (props) => {
   return (
     <Box>
-      <SbyButton variant="contained" size="xs">xs</SbyButton>
-      <SbyButton variant="contained" size="sm">sm</SbyButton>
-      <SbyButton variant="contained">normal</SbyButton>
-      <SbyButton variant="contained" size="lg">lg</SbyButton>
-      <SbyButton variant="contained">work on your next listing</SbyButton>
     </Box>
-
   );
 };
 
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -29,7 +29,7 @@ const theme = createMuiTheme({
     },
     MuiAppBar: {
       root: {
-        backgroundColor: 'transparent',
+        backgroundColor: '#FFF',
       },
     },
     MuiRadio: {
